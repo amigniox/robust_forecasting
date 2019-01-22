bac1e1aab170bebb8e063daff435dfaad9172545
amigniox <iamzxm@gmail.com>
2019-01-22 13:00:02 -0500

    Update save_json

--- src/models/preprocessor2.py
+++ src/models/preprocessor2.py
@@ -9,39 +9,25 @@ def save_json(start, end, path):
     if (end_date - start_date).days < 0:
         raise Exception('start date should NOT after end date')
 
-    ########## hardcode list here! #########
-    list = ['en.wikipedia.org',
-            'de.wikipedia.org',
-            'ja.wikipedia.org',
-            'zh.wikipedia.org',
-            'ru.wikipedia.org',
-            'commons.wikimedia.org',
-            'species.wikimedia.org',
-            'en.wiktionary.org',
-            'wikidata.org',
-            'en.wikinews.org',
-            'en.wikivoyage.org',
-            'wikisource.org',
-            'en.wikiversity.org',
-            'en.wikibooks.org',
-            'incubator.wikimedia.org',
-            'wikimediafoundation.org',
-            'outreach.wikimedia.org']
+    ########## provide wiki-project list here! #########
+    with open('wp.txt') as f:
+        wp_list = f.read().splitlines()
 
     # put dict of each domain to this list
     all_data = []
 
     delta = datetime.timedelta(days=199)
 
-    for item in list:
+    for item in wp_list:
         # wiki all projects' page views by human (not web scraping) per hour on a day
-        base_url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/aggregate/' + item + '/all-access/all-agents/hourly/'
+        base_url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/aggregate/' \
+                   + item \
+                   + '/all-access/all-agents/hourly/'
         start_date = datetime.datetime.strptime(start, '%Y%m%d')
 
         # dict is used to save JSON of each domain
         data = dict()
         data['start'] = start_date.strftime('%Y-%m-%d') + ' 00:00:00'
-        #data['start'] = start + '00'
         data['target'] = []
 
         while start_date <= end_date:
@@ -52,18 +38,23 @@ def save_json(start, end, path):
             print (final_url)  # debug
             # make a API request here!!!
             response = requests.get(final_url)
-            response.raise_for_status()
-            output = response.json()
-            print('processed data points: ' + str(len(output['items'])))  # debug
-            for i in range(len(output['items'])):
-                data['target'].append(output['items'][i]['views'])
+            try:
+                response.raise_for_status()
+                output = response.json()
+                print('processed data points: ' + str(len(output['items'])))  # debug
+                for i in range(len(output['items'])):
+                    data['target'].append(output['items'][i]['views'])
+            except requests.exceptions.HTTPError as e:
+                print("Error: " + str(e))
+                break
             start_date += datetime.timedelta(days=200)
-
-        all_data.append(data)
+        if data['target']:
+            all_data.append(data)
 
     # convert all items in list to JSON and write to a file
+    print('saving data')
     with open(path, 'w') as outfile:
         outfile.write('\n'.join(json.dumps(i) for i in all_data) +'\n')
 
 
-save_json('20150101', '20151003', 'data.json')
+save_json('20150101', '20161230', 'train.json')
