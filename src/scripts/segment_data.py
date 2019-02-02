6d1c4c430ab22471dc7c58d3c7454fa95c810389
amigniox <iamzxm@gmail.com>
2019-02-01 23:06:41 -0500

    Organize this weeks tests as notebooks.
    - data segmentation method based on workload types
    - examine the model prediction robustness across different types of
    workloads
    - hyperparameter tuning

--- src/scripts/segment_data.py
+++ src/scripts/segment_data.py
@@ -4,8 +4,9 @@ import numpy as np
 """
 This function characterize any given time series. 
 Basically it gives a score on the time series various aspects: trend, seasonality (if window = day, then seasonality =
-halfDay, day, week, month), the strongest characteristic will be the aspect with highest score; and 'spike' if the time
-series don't show any of the previous characteristics.
+halfDay, day, week, month), the strongest characteristic will be the aspect with highest score; 'DayImpulse' is introduced
+as a special time of day seasonality, it has a periodic impulse pattern and hard to predict compared with the smooth 
+cosine-like day seasonality; and 'spike' if the time series don't show any of the previous characteristics.
 
 The input will be Pandas series (with index = timestamp, and value = time series value) and window which simply defines the 
 unit (conversion between the desired cycle and the Nyquist frequency).
@@ -17,7 +18,7 @@ mean in time domain, standard deviation in time domain]
 
 CAUTION: sloppy scientific computing hard-coding style...
 
-For detailed demonstration of how it works, please see notebook 04-wiki-data-stats.ipynb
+For detailed demonstration of how it works, please see notebook 04-wiki-data-stats.ipynb and 05-wiki-data-segmentation.ipynb
 """
 
 
@@ -28,14 +29,14 @@ def characterize_ts(ts, window):
     k = np.arange(n)
     T = n / Fs
     frq = k / T  # two sides frequency range
-    # we only keep the positive frequency up to the Nyquist = 1/(2*dT), dT = sampling interval
+    # We only keep the positive frequency up to the Nyquist = 1/(2*dT), dT = sampling interval.
     cycle = frq[range(1, int(n / 2))] * window  # one side frequency range
 
     Y = np.fft.fft(y) / n  # fft computing and normalization
-    Y = Y[range(1, int(n / 2))]  # again, spectrum corresponding to the positive half
+    Y = Y[range(1, int(n / 2))]  # again, spectrum corrresponding to the positive half
     yabs = np.abs(Y)
 
-    # locate the largest 15 peaks, use them to characterise the time series
+    # Locate the largest 15 peaks, use them to characterise the time series.
     indx = heapq.nlargest(15, range(len(yabs)), yabs.__getitem__)
     amp = heapq.nlargest(15, yabs)
 
@@ -47,14 +48,14 @@ def characterize_ts(ts, window):
     cyc_month = cyc_day / 30.0
 
     comp = lambda a, b: np.abs(a / b - 1) < 0.05
-    ts_type = ['trend', 'halfDay', 'Day', 'Week', 'Month', 'spike']
+    ts_type = ['trend', 'hDay', 'Day', 'Week', 'Month', 'DayImpulse', 'spike']
     report_list = [0] * 8
     for counter, value in enumerate(indx):
-        # define a peak in frequency domain
+        # Here we define a peak in frequency domain.
         if amp[counter] > (mean + 3 * std):
             amp_norm = (amp[counter] - mean) / std
             if cycle[value] < 0.01:
-                # trend (increasing, decreasing, gaussian pulse)
+                # Trend (increasing, decreasing, gaussian pulse).
                 report_list[0] = max(amp_norm, report_list[0])
             elif comp(cycle[value], cyc_hday):
                 report_list[1] = max(amp_norm, report_list[1])
@@ -65,11 +66,21 @@ def characterize_ts(ts, window):
             elif comp(cycle[value], cyc_month):
                 report_list[4] = max(amp_norm, report_list[4])
 
-    if sum(report_list[:4]) > 0:
-        index = report_list[:4].index(max(report_list[:4]))
+    if sum(report_list[:5]) > 0:
+        index = report_list[:5].index(max(report_list[:5]))
         report_list[5] = ts_type[index]
     else:
-        report_list[5] = ts_type[-1:]
+        report_list[5] = ts_type[-1]
+
+    # Add a subcategory: a special day seasonality that has a periodic impulse shape.
+    if report_list[5] == 'hDay' or report_list[5] == 'Day':
+        harmonic = set(range(12))
+        all_peak = set()
+        for counter, value in enumerate(indx):
+            if amp[counter] > (mean + 3 * std):
+                all_peak.add(int(round(cycle[value])))
+        if len(harmonic.intersection(all_peak)) > 10:
+            report_list[5] = ts_type[-2]
 
     report_list[-2] = y.mean()
     report_list[-1] = y.std()
